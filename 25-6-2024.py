f6cb17f88b0094293be7c27c44cefa39f9d4bac7
karthik-s213 <karthiksubramanian213@gmail.com>
2024-06-25 20:35:34 +0530

    leetcode problem 25-6-24

---
+++ 25-6-2024.py
@@ -0,0 +1,10 @@
+# def __init__(self):
+#     self.sum=0
+# def bstToGst(self, root):
+#     if root:
+#         self.bstToGst(root.right)
+#         self.sum+=root.val
+#         root.val=self.sum
+#         self.bstToGst(root.left)
+        
+#     return root
